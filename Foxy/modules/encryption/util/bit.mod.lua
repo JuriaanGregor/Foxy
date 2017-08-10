8078130b5005da671c4e575ef3b1eec94c89588f
Juriaan <j.gregor@outlook.com>
2017-08-10 23:25:05 +0200

    Small typo

--- Foxy/modules/encryption/util/bit.mod.lua
+++ Foxy/modules/encryption/util/bit.mod.lua
@@ -1,10 +1,9 @@
-local bitmodule 	= require(game:GetService("ServerScriptService").Foxy.Encryption.modules.bitnumber);
 local root 			= game:GetService("ServerScriptService"):WaitForChild("Foxy");
-local application	= root:WaitForChild("Encryption");
+local module		= root.modules:WaitForChild("encryption");
+local application 	= module:WaitForChild("modules"):WaitForChild("bitnumber");
 
 if application then 
-	return require(application.modules.bitnumber);
+	return require(application);
 else 
 	error("[Foxy] Could not find a valid byte library. Please use the Plugin to install the correcy package.", 2);
 end 
-
